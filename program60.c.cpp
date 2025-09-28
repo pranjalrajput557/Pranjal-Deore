5e197da6b36aa73ba94b9979ff87d9491d7978f4
pranjalrajput557 <pranjalrajput557@gmail.com>
2025-09-29 00:05:59 +0530

    Add files via upload

---
+++ program60.c.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+    int a = 5, b = 10;
+    printf("First number: %d\n", a);
+    printf("Second number: %d\n", b);
+    return 0;
+}
---
+++ program69.c.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() {
+    int age = 20;
+    printf("My age is %d\n", age);
+    return 0;
+}
